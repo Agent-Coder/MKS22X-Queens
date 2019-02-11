9b77e90e49c1d955c166765a25ff52a5549a3fd8
Amanda Zheng <azheng00@stuy.edu>
2019-02-10 22:29:44 -0500

    fixed add Queen

--- QueenBoard.java
+++ QueenBoard.java
@@ -18,36 +18,53 @@ public class QueenBoard{
       for (int i=0;i<board.length;i++){
         s+="\n";
         for (int j=0;j<board.length;j++){
-          if (board[i][j]==0){
-            s+="_";
-          }
-          else{
-            s+="Q";
-          }
+          //if (board[i][j]!=-1){
+            s+=board[i][j];
+          //}
+          //else{
+            //s+="Q";
+          //}
       }
     }
+    return s;
   }
   public boolean solve(){
+    for(int i=0;i<board.length;i++){
+      for(int h=0;h<board.length;h++){
+        if (board[i][h]!=0){
+          throw new IllegalStateException();
+        }
+      }
+    }
     return solution(board.length,0,0);
   }
   public boolean solution(int size,int r,int c){
-    if (r==c&&c==board.length){
-      if(size==0){
+    if (r==board.length||c==board.length||r<0){
+      if(size<0){
         return true;
       }
       else{
         return false;
       }
     }
+    System.out.println("begin"+size+" "+r+" "+c);
     if(addQueen(r,c)){
-      return solution(size,r+1,c);
+      return solution(size-1,r+1,0);
     }
     else{
-      if (c==board.length){
-        removeQueen(r,c)
+      System.out.println("remove"+r+" "+c);
+      for (int k=0;k<board.length;k++) {
+        if(r!=0&&board[r][k]==-1){
+          System.out.println("removing"+(r-1)+" "+c);
+          removeQueen(r,k);
+          c=k;
+          }
+        }
+        if(c+1==board.length){
+            return solution(size,r-1,0);
+        }
+        return solution(size,r,c+1);
       }
-      return solution(size,r,c+1);
-    }
   }
   private boolean addQueen(int r,int c){
     if (board[r][c]!=0){
@@ -55,14 +72,23 @@ public class QueenBoard{
     }
     for (int i=0;i<board.length;i++){
       board[r][i]=board[r][i]+1;
-      board[i][c]=board[i][c]+1;
+      if(r!=c){
+        board[i][c]=board[i][c]+1;
+      }
     }
     int a=0;
-    int b=0;
+    int b=c-r;
     while(a<board.length&&b<board.length){
       board[a][b]=board[a][b]+1;
       a+=1;
       b+=1;
+    }
+    a=0;
+    b=c+r;
+    while(a<board.length&&b>=0){
+      board[a][b]=board[a][b]+1;
+      a+=1;
+      b-=1;
     }
     board[r][c]=-1;
     return true;
@@ -86,5 +112,10 @@ public class QueenBoard{
       b+=1;
     }
     return true;
+  }
+  public static void main(String[] args) {
+    QueenBoard x=new QueenBoard(8);
+    x.addQueen(1,3);
+    System.out.println(x);
   }
 }
